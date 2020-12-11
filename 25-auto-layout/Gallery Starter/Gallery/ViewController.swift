157b69db7004b332fb51ec17b88477c1b7c02b85
Tk <timkaldybek@gmail.com>
2020-12-11 19:28:47 +0600

    Understandig auto layout

--- 25-auto-layout/Gallery Starter/Gallery/ViewController.swift
+++ 25-auto-layout/Gallery Starter/Gallery/ViewController.swift
@@ -6,12 +6,5 @@ class ViewController: UIViewController {
     super.viewDidLoad()
   }
   
-  @IBAction func buttonTapped(_ sender: UIButton) {
-    if sender.title(for: .normal) == "X" {
-      sender.setTitle("A very long title for this button", for: .normal)
-    } else {
-      sender.setTitle("X", for: .normal)
-    }
-  }  
   
 }
